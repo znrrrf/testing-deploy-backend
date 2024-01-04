24f2ecd75c77e236ff37574d19459f44dc528cd6
zainur rouf <zainurrouf4@gmail.com>
2024-01-04 20:20:48 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.use("/", (req, res) => {
+  try {
+    res.status(200).send({
+      message: "this is my api",
+    });
+  } catch (error) {
+    res.status(400).send({
+      error,
+    });
+  }
+});
+
+app.listen(1000, () => {
+  console.log("por is running on 1000");
+});
