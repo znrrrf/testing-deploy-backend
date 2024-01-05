0f350aec9a212d24dfcc6b4ff84b5084e243bce6
zainur rouf <zainurrouf4@gmail.com>
2024-01-05 13:29:37 +0700

    delete some

--- models/index.js
+++ models/index.js
@@ -15,13 +15,10 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else if (config.uri) {
-  sequelize = new Sequelize(config.uri, {
-    dialectModule: require("mysql2"),
-  });
+  sequelize = new Sequelize(config.uri);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, {
     ...config,
-    dialectModule: require("mysql2"),
   });
 }
 
