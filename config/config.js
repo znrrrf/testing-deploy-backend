a17b88f2d437221c1dd884e2dca0fc6192988a69
zainur rouf <zainurrouf4@gmail.com>
2024-01-05 10:29:28 +0700

    fix error env

--- config/config.js
+++ config/config.js
@@ -1,27 +1,28 @@
 const dotenv = require("dotenv");
 dotenv.config();
+// console.log({ env: process.env.USER_NAME });
 module.exports = {
   development: {
-    // username: process.env.USERNAME,
-    // password: process.env.PASSWORD,
-    // database: process.env.DATABASE,
-    // host: process.env.HOST,
-    // dialect: process.env.DIALECT,
-    username: "root",
-    password: "root",
-    database: "testing-db",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.USER_NAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+    // username: "root",
+    // password: "root",
+    // database: "testing-db",
+    // host: "127.0.0.1",
+    // dialect: "mysql",
   },
   test: {
-    username: process.env.USERNAME,
+    username: process.env.USER_NAME,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     host: process.env.HOST,
     dialect: process.env.DIALECT,
   },
   production: {
-    username: process.env.USERNAME,
+    username: process.env.USER_NAME,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     host: process.env.HOST,
--- models/index.js
+++ models/index.js
@@ -7,29 +7,30 @@ const process = require("process");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
+// console.log({ config });
+
 const db = {};
 
-// let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(
-//     config.database,
-//     config.username,
-//     config.password,
-//     config
-//   );
-// }
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    ...config,
+    dialectModule: require("mysql2"),
+    benchmark: true,
+  });
+}
 
-const sequelize = new Sequelize({
-  host: "bg0gktkdgouzpen9spo9-mysql.services.clever-cloud.com",
-  username: "usepslyiyuw2ni5p",
-  password: "sw4tvwGwf4JJ7ymgR6so",
-  database: "bg0gktkdgouzpen9spo9",
-  dialect: "mysql",
-  dialectModule: require("mysql2"),
-  benchmark: true,
-});
+// const sequelize = new Sequelize({
+//   host: "bg0gktkdgouzpen9spo9-mysql.services.clever-cloud.com",
+//   username: "usepslyiyuw2ni5p",
+//   password: "sw4tvwGwf4JJ7ymgR6so",
+//   database: "bg0gktkdgouzpen9spo9",
+//   dialect: "mysql",
+//   dialectModule: require("mysql2"),
+//   benchmark: true,
+// });
 
 fs.readdirSync(__dirname)
   .filter((file) => {
