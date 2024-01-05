56306b2bfcd5335ea570d562669cd759ce1e3d4a
zainur rouf <zainurrouf4@gmail.com>
2024-01-05 10:00:29 +0700

    set error

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -10,10 +10,14 @@ module.exports = {
         result,
       });
     } catch (error) {
+      res.status(400).send({
+        message: "Internal Server Error",
+        error,
+      });
       console.error("Error in getUser controller:", error);
       res.status(500).send({
         message: "Internal Server Error",
-        error: error.message,
+        error,
       });
     }
   },
