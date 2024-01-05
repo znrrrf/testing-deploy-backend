e765e90b4d691b582f1a639a77d2e16a9c7888b8
zainur rouf <zainurrouf4@gmail.com>
2024-01-05 10:34:37 +0700

    set another log

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -4,6 +4,8 @@ const user = db.User;
 module.exports = {
   getUser: async (req, res) => {
     try {
+      console.log({ db });
+
       const result = await user.findAll();
 
       res.status(200).send({
